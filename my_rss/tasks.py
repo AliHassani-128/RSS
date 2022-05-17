98f5b6517031f1fd619cc452787bf947257814ab
AliHassani <alihassani128@yahoo.com>
2022-05-17 23:16:06 +0430

    fix urls and pagination setting

--- my_rss/tasks.py
+++ my_rss/tasks.py
@@ -1,6 +1,18 @@
 from celery import shared_task
-
+'''
+from rss.models import RSS, NEWS
+from datetime import datetime
 
 @shared_task
 def insert_news():
-    pass
+    last_news = NEWS.objects.last()
+    if last_news:
+        last_rss = last_news.rss.all()[-1]
+        rsses = RSS.objects.filter(time__lt=last_rss.time)
+        new_news = NEWS.objects.create()
+        new_news.rss.add(*rsses)
+    else:
+        rsses = RSS.objects.filter(time__lt=datetime.now())
+        new_news = NEWS.objects.create()
+        new_news.rss.add(*rsses)
+    '''
